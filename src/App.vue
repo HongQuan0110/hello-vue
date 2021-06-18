5b105f9e7542e77d6b525fd0bb01eb796daef120
HQuaan <lvhquan0110@gmail.com>
2021-06-18 23:52:58 +0700

    init vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,26 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu />
+    <input type="text" v-model="name" />
+    {{name}}
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      name: "Vue"
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Menu
   }
 }
 </script>
